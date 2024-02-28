abae1ecfbda68d5bcdb8032e24f1fde56898699b
Luisbarbosa17 <153692802+Luisbarbosa17@users.noreply.github.com>
2024-02-27 21:54:46 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const display = document.getElementById("resultado")
+console.log(display)
+
+function inserirdisplay(valor){
+    display.value+=valor
+}
+
+function limpardisplay(){
+    display.value=""
+}
+
+function calcular(){
+    display.value = eval(display.value)
+    
+}
