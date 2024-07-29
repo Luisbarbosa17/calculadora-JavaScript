06af921217dd70b494b713a9b1219a305cf084f8
Luis Barbosa <153692802+Luisbarbosa17@users.noreply.github.com>
2024-07-29 12:31:29 -0300

    Update scripts.js
    alteração devido ao erro durante o salvamento/download

--- scripts.js
+++ scripts.js
@@ -4,12 +4,9 @@ console.log(display)
 function inserirdisplay(valor){
     display.value+=valor
 }
-
 function limpardisplay(){
     display.value=""
 }
-
 function calcular(){
     display.value = eval(display.value)
-    
 }
